a443d0ec8b5de3321e81a9d7869fddfe69d2b5c8
Evgeny Rachinskiy <rachinsky@gmail.com>
2013-10-05 14:50:03 +0200

    add eclipse project metadata

---
+++ src-gen/cnd/pojo/TestNodeType3.java
@@ -0,0 +1,21 @@
+package cnd.pojo;
+
+public class TestNodeType3 {
+	private String propStr;        		
+	private Boolean propBool;        		
+
+	public String getPropStr(){
+	    return this.propStr;
+	}        		
+	public void setPropStr(String propStr){
+	    this.propStr = propStr;
+	}        		
+	
+	public Boolean getPropBool(){
+	    return this.propBool;
+	}        		
+	public void setPropBool(Boolean propBool){
+	    this.propBool = propBool;
+	}        		
+	
+}
